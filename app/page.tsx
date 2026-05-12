240547447cbe507efc8a3469a92f4fb0d2cdc88f
imsg1006 <shlokgupta10@gmail.com>
2026-05-12 22:11:19 +0530

    changed the position of projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 import Hero from "@/components/Hero"
-import { FloatingNav } from "@/components/ui/FloatingNav" 
+import { FloatingNav } from "@/components/ui/FloatingNav"
 import Grid from "@/components/Grid"
 import { navItems } from "@/data"
 import Experience from "@/components/Experience"
-import RecentProjects from "@/components/RecentProjects" 
+import RecentProjects from "@/components/RecentProjects"
 import Footer from "@/components/Footer"
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-      <FloatingNav 
-      navItems={navItems}
-      />
-         <Hero />
-         <Grid/>
-         <RecentProjects/> 
-         <Experience/>
-         {/* <Approach/> */}
-         <Footer/>
+        <FloatingNav
+          navItems={navItems}
+        />
+        <Hero />
+        <RecentProjects />
+        <Experience />
+        <Grid />
+        {/* <Approach/> */}
+        <Footer />
       </div>
     </main>
   )
